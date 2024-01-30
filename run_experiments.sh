363f54d6860dcf6c83478cda9dd325822186eb0f
Helge Spieker <helge@simula.no>
2024-01-30 09:45:40 +0100

    Experiment commands

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# python script/representation.py x264 -p kbs -m original --data-dir data --output results
-# python script/representation.py x264 -p kbs -m tsne -d 3 --data-dir data --output results
-# python script/representation.py x264 -p kbs -m pca -d 3 --data-dir data --output results
-# python script/representation.py x264 -p kbs -m embed -d 3 --data-dir data --output results
-# python script/representation.py x264 -p kbs -m embed -d 32 --data-dir data --output results
+python script/representation.py x264 kbs -m original --data-dir data --output results
+python script/representation.py x264 kbs -m tsne -d 3 --data-dir data --output results
+python script/representation.py x264 kbs -m pca -d 3 --data-dir data --output results
+python script/representation.py x264 kbs -m embed -d 3 --data-dir data --output results
+python script/representation.py x264 kbs -m embed -d 32 --data-dir data --output results
 
-python script/representation.py poppler -p size -m original --data-dir data --output results
-python script/representation.py poppler -p size -m tsne -d 3 --data-dir data --output results
-python script/representation.py poppler -p size -m pca -d 3 --data-dir data --output results
-python script/representation.py poppler -p size -m embed -d 3 --data-dir data --output results
-python script/representation.py poppler -p size -m embed -d 32 --data-dir data --output results
+python script/representation.py gcc size -m original --data-dir data --output results
+python script/representation.py gcc size -m tsne -d 3 --data-dir data --output results
+python script/representation.py gcc size -m pca -d 3 --data-dir data --output results
+python script/representation.py gcc size -m embed -d 3 --data-dir data --output results
+python script/representation.py gcc size -m embed -d 32 --data-dir data --output results
+
+python script/representation.py imagemagick size -m original --data-dir data --output results
+python script/representation.py imagemagick size -m tsne -d 3 --data-dir data --output results
+python script/representation.py imagemagick size -m pca -d 3 --data-dir data --output results
+python script/representation.py imagemagick size -m embed -d 3 --data-dir data --output results
+python script/representation.py imagemagick size -m embed -d 32 --data-dir data --output results
+
+python script/representation.py poppler size -m original --data-dir data --output results
+python script/representation.py poppler size -m tsne -d 3 --data-dir data --output results
+python script/representation.py poppler size -m pca -d 3 --data-dir data --output results
+python script/representation.py poppler size -m embed -d 3 --data-dir data --output results
+python script/representation.py poppler size -m embed -d 32 --data-dir data --output results
+
