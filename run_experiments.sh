69d2a0f1545b3f96bbd698069c4f47adb2336943
Helge Spieker <info@hspieker.de>
2024-02-01 08:51:45 +0100

    Experiment script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-python script/representation.py x264 kbs -m original --data-dir data --output results
-python script/representation.py x264 kbs -m tsne -d 3 --data-dir data --output results
-python script/representation.py x264 kbs -m pca -d 3 --data-dir data --output results
-python script/representation.py x264 kbs -m embed -d 3 --data-dir data --output results
-python script/representation.py x264 kbs -m embed -d 32 --data-dir data --output results
+
+# python script/representation.py gcc size -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py imagemagick size -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py lingeling conflicts -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py nodejs ops -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py poppler size -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py sqlite q1 -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py x264 kbs -m embed -d 3 --data-dir data --output results --epochs=30
+# python script/representation.py xz size -m embed -d 3 --data-dir data --output results --epochs=30
+
+
+# python script/representation.py x264 kbs -m original --data-dir data --output results
+# python script/representation.py x264 kbs -m tsne -d 3 --data-dir data --output results
+# python script/representation.py x264 kbs -m pca -d 3 --data-dir data --output results
+# python script/representation.py x264 kbs -m embed -d 3 --data-dir data --output results
+# python script/representation.py x264 kbs -m embed -d 32 --data-dir data --output results
 
 python script/representation.py gcc size -m original --data-dir data --output results
 python script/representation.py gcc size -m tsne -d 3 --data-dir data --output results
